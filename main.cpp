d4b71e13eaba19859c0eb714f7341d959be3f0ec
JuanDiegoSusunaga <138306321+JuanDiegoSusunaga@users.noreply.github.com>
2025-03-31 19:56:40 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,39 @@
 /*
- * Archivo: actividad_2_1.cpp
- * Descripción: Implementación de una función de hash polinómico para calcular el valor hash de una cadena.
+ * Descripción: Pruebas unitarias para la función de hash polinómico.
  * Autor: Juan Diego Susunaga
  * Matrícula: A01764884
  * Fecha: 31/03/2025
  */
+
+#include "gtest/gtest.h"
 #include <iostream>
 #include <string>
-
 using namespace std;
 
 long long prhf(string s) {
- int n = s.length();
- int p = 31;
- int m = 1e9 + 9;
- long long valorHash = 0;
- long long potencia = 1;
+    int n = s.length();
+    int p = 31;
+    int m = 1e9 + 9;
+    long long valorHash = 0;
+    long long potencia = 1;
 
- for (int i = 0; i < n; i++) {
-  valorHash = (valorHash + (s[i] - 'a' + 1) * potencia) % m;
-  potencia = (potencia * p) % m;
- }
- return valorHash;
+    for (int i = 0; i < n; i++) {
+        valorHash = (valorHash + (s[i] - 'a' + 1) * potencia) % m;
+        potencia = (potencia * p) % m;
+    }
+    return valorHash;
 }
 
-int main() {
- string S;
- getline(cin, S);
- cout << prhf(S) << endl;
- return 0;
+// Prueba con casos básicos
+TEST(HashFunctionTest, BasicCases) {
+    EXPECT_EQ(prhf("abc"), 2946);
+    EXPECT_EQ(prhf("hello"), 99162322);
+    EXPECT_EQ(prhf("test"), 120129);
+}
+
+
+// Función main para ejecutar las pruebas
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
